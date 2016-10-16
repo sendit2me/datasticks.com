3dde00be98748209dc0333ce4fa6a7d78b577281
cfregly <chris@fregly.com>
2016-10-16 03:29:15 +0000

    updated order

--- datasticks-up.sh
+++ datasticks-up.sh
@@ -1,5 +1,14 @@
-echo '...Kubernetes Admin UI (Weave Scope)...'
-kubectl create -f dashboard.ml/weavescope/weavescope.yaml
+'...MySql...'
+kubectl create -f sql.ml/mysql-rc.yaml
+kubectl create -f sql.ml/mysql-svc.yaml
+
+echo '...ZooKeeper...'
+kubectl create -f zookeeper.ml/zookeeper-rc.yaml
+kubectl create -f zookeeper.ml/zookeeper-svc.yaml
+
+echo '...Dynomite + Redis...'
+kubectl create -f dynomite.ml/dynomite-rc.yaml
+kubectl create -f dynomite.ml/dynomite-svc.yaml
 
 echo '...Spark Master...'
 kubectl create -f apachespark.ml/spark-master-rc.yaml
@@ -17,33 +26,28 @@ echo '...JupyterHub...'
 kubectl create -f jupyterhub.ml/jupyterhub-rc.yaml
 kubectl create -f jupyterhub.ml/jupyterhub-svc.yaml
 
-echo '...MySql...'
-kubectl create -f sql.ml/mysql-rc.yaml
-kubectl create -f sql.ml/mysql-svc.yaml
-
 echo '...Airflow Scheduler...'
 kubectl create -f scheduler.ml/airflow-rc.yaml
 kubectl create -f scheduler.ml/airflow-svc.yaml
-
-echo '...ZooKeeper...'
-kubectl create -f zookeeper.ml/zookeeper-rc.yaml
-kubectl create -f zookeeper.ml/zookeeper-svc.yaml
 
 echo '...Kafka...'
 kubectl create -f stream.ml/kafka-rc.yaml
 kubectl create -f stream.ml/kafka-svc.yaml
 
-echo '...Dynomite + Redis...'
-kubectl create -f dynomite.ml/dynomite-rc.yaml
-kubectl create -f dynomite.ml/dynomite-svc.yaml
+echo '...NetflixOSS Core Components...'
+kubectl create -f serve.ml/netflix-rc.yaml
+kubectl create -f serve.ml/netflix-svc.yaml
 
-echo '...Prediction Serving Layer (JVM-based)...'
-kubectl create -f serve.ml/jvm-rc.yaml
-kubectl create -f serve.ml/jvm-svc.yaml
+echo '...NetflixOSS-based Prediction Services...'
+kubectl create -f serve.ml/prediction-rc.yaml
+kubectl create -f serve.ml/prediction-svc.yaml
 
-echo '...Prediction Serving Layer (TensorFlow-based)...'
+echo '...TensorFlow Serving-based Prediction Services...'
 kubectl create -f serve.ml/tensorflow-rc.yaml
 kubectl create -f serve.ml/tensorflow-svc.yaml
+
+echo '...Dashboard...'
+kubectl create -f dashboard.ml/kubernetes/weavescope.yaml
 
 echo '...Apache...'
 kubectl create -f apache.ml/apache-rc.yaml
