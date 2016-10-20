52321c00fca84649fd35afa1bb068e14e3629507
cfregly <chris@fregly.com>
2016-10-20 09:42:51 +0000

    updated up script

--- datasticks-up.sh
+++ datasticks-up.sh
@@ -22,6 +22,10 @@ echo '...Spark Worker...'
 kubectl create -f apachespark.ml/spark-worker-rc.yaml
 kubectl create -f apachespark.ml/spark-worker-svc.yaml
 
+echo '...Hive Metastore...'
+kubectl create -f metastore.ml/metastore-rc.yaml
+kubectl create -f metastore.ml/metastore-svc.yaml
+
 echo '...Zeppelin...'
 kubectl create -f zeppelin.ml/zeppelin-rc.yaml
 kubectl create -f zeppelin.ml/zeppelin-svc.yaml
@@ -33,6 +37,18 @@ kubectl create -f jupyterhub.ml/jupyterhub-svc.yaml
 echo '...Airflow Scheduler...'
 kubectl create -f scheduler.ml/airflow-rc.yaml
 kubectl create -f scheduler.ml/airflow-svc.yaml
+
+echo '...Presto Master...'
+kubectl create -f presto.ml/presto-master-rc.yaml
+kubectl create -f presto.ml/presto-master-svc.yaml
+
+echo '...Presto Worker...'
+kubectl create -f presto.ml/presto-worker-rc.yaml
+kubectl create -f presto.ml/presto-worker-svc.yaml
+
+echo '...Presto AirPal...'
+kubectl create -f presto.ml/airpal-rc.yaml
+kubectl create -f presto.ml/airpal-svc.yaml
 
 echo '...Kafka...'
 kubectl create -f stream.ml/kafka-rc.yaml
